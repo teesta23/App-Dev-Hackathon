6c6f7894d901584cff789d5ce02fe89993cfdd1f
Michael McCarroll <michaelmccarroll@Michaels-MacBook-Air-8.local>
2025-11-26 17:33:49 -0500

    added some styling stuff to the main page to give an idea of some of the fundamental styles of the website (font, what buttons will look like, background for some pages)

--- bootcamp-project/src/App.tsx
+++ bootcamp-project/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -16,10 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 id="header">code more</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button id="start">
+          <div id="getstarted">&gt; get started</div>
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
